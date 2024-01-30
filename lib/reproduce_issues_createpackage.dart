4db5aa5f2c90f740b5fee92fc05f2f245f3b9b5a
Nguyen Huy <huy@nevercode.io>
2024-01-30 17:10:19 +0700

    first commit

---
+++ lib/reproduce_issues_createpackage.dart
@@ -0,0 +1,3 @@
+library reproduce_issues_createpackage;
+
+export 'src/reproduce_issues_createpackage.dart';
---
+++ lib/src/reproduce_issues_createpackage.dart
@@ -0,0 +1,11 @@
+class Calculator {
+
+	Calculator(this.value);
+
+  int value;
+
+  /// Returns [value] plus 1.
+  int addOne() {
+    return value += 1;
+  }
+}
